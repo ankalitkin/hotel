b4be859b558abf76f3697fb3f5d2a5535736cc08
AndreqGav <Andreq.Gav@mail.ru>
2019-07-30 19:10:32 +0300

    Добавил получение пользователем свою историю проживания(и бронирования по факту)

---
+++ Hotel/ClientApp/src/app/transaction/pages/user-transaction-page/user-transaction-page.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { DataServiceTransaction } from '../../services/data.service.transaction';
+import { Transaction } from '../../models/transaction';
+
+import { FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-transaction-page',
+  templateUrl: './user-transaction-page.component.html',
+  styleUrls: ['./user-transaction-page.component.scss'],
+  providers: [DataServiceTransaction]
+})
+export class UserTransactionPageComponent implements OnInit {
+
+  transactions?: Transaction[];
+  isLoaded: Boolean = false;
+
+  constructor(private dataService: DataServiceTransaction, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.loadUserTransaction();
+  }
+
+  loadUserTransaction() {
+    this.isLoaded = false;
+
+    this.dataService.GetMyHistory()
+        .subscribe((data: Transaction[]) => { this.CompleteLoad(data); });
+  }
+
+  CompleteLoad(data: Transaction[]) {
+    this.transactions = data;
+    for (let elem in this.transactions) {
+      this.transactions[elem].TheNoumber = +elem + 1;
+    }
+
+    this.isLoaded = true;
+    console.log(this.transactions);
+  }
+
+}
---
+++ Hotel/ClientApp/src/app/transaction/user-transaction/user-transaction.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Transaction } from '../models/transaction';
+
+
+@Component({
+  selector: 'user-transaction',
+  templateUrl: './user-transaction.component.html',
+  styleUrls: ['./user-transaction.component.scss']
+})
+export class UserTransactionComponent implements OnInit {
+
+  displayedColumns: string[] = ['TheNoumber', 'ComeIn', 'ComeOut', 'cost'];
+  dataSource: MatTableDataSource<Transaction>;
+  @Input()
+  transactions: Transaction[] | null | undefined;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(this.transactions);
+
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
