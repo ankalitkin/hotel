66ddf17a313c41ed8621251a217eedadfa43f19a
Armen <armentovmasyan02@gmail.com>
2019-08-03 02:08:15 +0300

    Добавил UserProfile и Auth,изменил JWT код (не всё сделанно,еще должен добавить нормальный дизайн и добавить видимость для кнопок)

---
+++ Hotel/ClientApp/src/app/user/userhome/userhome.component.ts
@@ -0,0 +1,28 @@
+
+import { UserService } from 'src/app/user/services/user.service';
+
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-userhome',
+  templateUrl: './userhome.component.html',
+  styleUrls: ['./userhome.component.scss']
+})
+export class UserhomeComponent implements OnInit {
+
+  userDetails;
+  constructor(private router: Router, private service: UserService) { }
+  ngOnInit() {
+    this.service.getUserProfile().subscribe(
+      res => {
+        this.userDetails = res;
+      },
+      err => {
+        console.log(err);
+        console.log("!!!");
+      },
+    );
+  }
+
+}
---
+++ Hotel/models/reg_model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class reg_model
+    {
+        public string Firstname { get; set;}
+        public string Lastname { get; set; }
+        public string Password{ get; set; }
+        public string Email { get; set; }
+
+        public reg_model(string f, string l,string p,string e)
+        {
+            Firstname = f;
+            Lastname = l;
+            Password = p;
+            Email=e;
+        }
+    }
+}
